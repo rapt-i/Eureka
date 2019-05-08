7f8347fe100859d20a438eac395823b33787a5af
C-uo <chouyuou@gmail.com>
2019-05-08 14:17:31 +0900

    完成了Summary的模型(仮)

--- summary/models.py
+++ summary/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+# from aircraft.models import TestFlightType
+
+
+class Summary(models.Model):
+    time = models.DateTimeField(verbose_name='日付', default=now)
+    name = models.CharField(verbose_name='氏名', max_length=50)
+    tf = models.ForeignKey(
+        aircraft.TestFlightType,
+        verbose_name='TFタイプ',
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+
+
+class Section(models.Model):
+    STATUS_CHOICES = (
+        (0, '事前準備'),
+        (1, '重心試験'),
+        (2, '回転試験'),
+        (3, '機体搬出'),
+        (4, '組み立て'),
+        (5, '転がし試験'),
+        (6, '滑走試験'),
+        (7, 'ジャンプ試験'),
+        (8, '飛行試験'),
+        (9, '解体撤収'),
+        (10, 'その他')
+    )
+    title = models.CharField(verbose_name='類名', choices=STATUS_CHOICES, default=10)
+    advantage = models.CharField(verbose_name='良かった点', max_length=2000, blank=True)
+    disadvantage = models.CharField('反省点', max_length=2000, blank=True)
+    improvement = models.CharField('改善案', max_length=2000, blank=True)
+    summary = models.ForeignKey(
+        Summary,
+        verbose_name='総括',
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    )
